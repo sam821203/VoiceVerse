bb0af0bda6388991d6ed1624bb8b36476ce8f951
yuhao <sam821203@yahoo.com.tw>
2023-09-13 11:54:58 +0800

    docs: add pwa mode & manifest setting

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,31 @@ import { fileURLToPath, URL } from 'node:url'
 import vueSetupExtend from 'vite-plugin-vue-setup-extend'
 
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(), vueSetupExtend()
+    vue(),
+    vueSetupExtend(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
+      manifest: {
+        name: 'VoiceVerse',
+        theme_color: '#ff5e3a',
+        icons: [
+          {
+            src: 'assets/img/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
   ],
   resolve: {
     alias: {
