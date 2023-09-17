87e22dd7569ebf576ab817c21f3a5e63703095c3
SamHuang <sam821203@yahoo.com.tw>
2023-09-18 07:49:26 +0800

    style: home page

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.lj3cin85sug"
+    "revision": "0.vdg6booielo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,10 +1,10 @@
 <template>
   <!-- Header -->
-  <header id="header" class="bg-gray-700 mx-auto">
+  <header id="header" class="bg-zinc-100 border-b border-zinc-300 mx-auto">
     <nav class="container mx-auto flex justify-start items-center py-5 px-4 max-w-6xl">
       <!-- App Name -->
       <router-link
-        class="text-white font-bold uppercase text-2xl mr-4"
+        class="text-black font-bold uppercase text-2xl mr-4"
         :to="{ name: 'home' }"
         exact-active-class="no-active"
         >Music</router-link
@@ -13,26 +13,26 @@
         <!-- Primary Navigation -->
         <ul class="flex flex-row mt-1">
           <li>
-            <router-link class="px-2 text-white" :to="{ name: 'about' }">About</router-link>
+            <router-link class="px-2 text-black" :to="{ name: 'about' }">About</router-link>
           </li>
           <!-- Navigation Links -->
           <li v-if="!userLoggedIn">
-            <a class="px-2 text-white" href="#" @click.prevent="toggleAuthModal"
+            <a class="px-2 text-black" href="#" @click.prevent="toggleAuthModal"
               >Login / Register</a
             >
           </li>
           <template v-else>
             <li>
-              <router-link class="px-2 text-white" :to="{ name: 'manage' }">Manage</router-link>
+              <router-link class="px-2 text-black" :to="{ name: 'manage' }">Manage</router-link>
             </li>
             <li>
-              <a class="px-2 text-white" href="#" @click.prevent="signingOut">Logout</a>
+              <a class="px-2 text-black" href="#" @click.prevent="signingOut">Logout</a>
             </li>
           </template>
         </ul>
         <ul class="ml-auto">
           <li>
-            <a href="#" class="px-2 text-white" @click.prevent="changeLocale">{{
+            <a href="#" class="px-2 text-black" @click.prevent="changeLocale">{{
               currentLocale
             }}</a>
           </li>
